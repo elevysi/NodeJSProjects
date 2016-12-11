71bd2141d9f684217e8fd3440ae6f753fc8c59a6
elevysi <starstories@Stars-iMac.fritz.box>
2016-12-11 19:21:28 +0100

    Photos App Start

---
+++ photo/models/Photo.js
@@ -0,0 +1,9 @@
+var mangoose = require('mongoose');
+mongoose.connect('mongodb://localhost/photo_app');
+
+var schema = new mongoose.Schema({
+	name : String,
+	path : String
+});
+
+module.exports = mongoose.model('Photo', schema);
